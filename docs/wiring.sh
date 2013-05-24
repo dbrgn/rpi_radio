4ded753107ead24ee31ca249b1a1ce240e12548d
Danilo Bargen <gezuru@gmail.com>
2013-05-24 11:11:35 +0200

    Resized wiring.png

--- docs/wiring.sh
+++ docs/wiring.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pdflatex wiring.tex && \
-convert -flatten -density 300 wiring.pdf -quality 90 -resize 712x712 wiring_small.png
+convert -flatten -density 300 wiring.pdf -quality 90 -resize 920x920 wiring.png
