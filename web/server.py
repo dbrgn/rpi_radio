2b86d179b55a5dc4ffd00ece6b69cc9a41532f45
Danilo Bargen <gezuru@gmail.com>
2013-05-27 16:35:57 +0200

    Web frontend: Some more stuff

--- web/server.py
+++ web/server.py
@@ -23,7 +23,7 @@ from __future__ import print_function, division, absolute_import, unicode_litera
 
 from contextlib import closing
 
-from flask import Flask, render_template
+from flask import Flask, url_for, redirect, render_template
 import yaml
 
 CONFIG = 'config.yml'
@@ -33,9 +33,29 @@ app = Flask(__name__)
 
 ### Views ###
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template('home.html')
+    return redirect(url_for('dashboard'))
+
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/config/wlan/')
+def config_wlan():
+    return render_template('config_wlan.html')
+
+
+@app.route('/config/radio/')
+def config_radio():
+    return render_template('config_radio.html')
+
+
+@app.route('/config/alarm/')
+def config_alarm():
+    return render_template('config_alarm.html')
 
 
 ### Dev Server ###
