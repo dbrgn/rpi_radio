00ef41c70b19e0f6bf21610e9634ab7973b4d58a
Danilo Bargen <gezuru@gmail.com>
2013-05-27 18:45:58 +0200

    Config parsing and WLAN config page (readonly)

--- web/server.py
+++ web/server.py
@@ -21,14 +21,25 @@ Configuration variables:
 """
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-from contextlib import closing
-
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, g, url_for, redirect, render_template
 import yaml
 
 CONFIG = 'config.yml'
 
 app = Flask(__name__)
+
+
+### Config parsing ###
+
+def get_config():
+    """Open and parse config file."""
+    with open(CONFIG) as configfile:
+        return yaml.safe_load(configfile)
+
+
+@app.before_request
+def before_request():
+    g.config = get_config()
 
 
 ### Views ###
@@ -45,20 +56,20 @@ def dashboard():
 
 @app.route('/config/wlan/')
 def config_wlan():
-    return render_template('config_wlan.html')
+    return render_template('config_wlan.html', config=g.config.get('wlan'))
 
 
 @app.route('/config/radio/')
 def config_radio():
-    return render_template('config_radio.html')
+    return render_template('config_radio.html', config=g.config.get('radio'))
 
 
 @app.route('/config/alarm/')
 def config_alarm():
-    return render_template('config_alarm.html')
+    return render_template('config_alarm.html', config=g.config.get('alarm'))
 
 
 ### Dev Server ###
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
